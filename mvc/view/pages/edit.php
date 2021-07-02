874fe60897989d79873f53655d8dc16151471790
Hiếu JB <trunghieuit061099@gmail.com>
2021-07-02 10:50:05 +0700

    create Edit page

---
+++ mvc/view/pages/edit.php
@@ -0,0 +1,220 @@
+<section class="text-gray-600 body-font mb-16">
+    <div class="container px-5  mx-auto">
+        <div class="flex flex-col text-center w-full mb-20">
+            <h1
+                class="text-blue-900 sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 hover:text-red-700 cursor-pointer">
+                QUẢN LÝ
+                THÔNG TIN </h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Vui lòng đưa chuột đến các vị trí để thực hiện
+                chỉnh sửa thông tin môn học, giáo viên, khoa, sinh viên và điểm.</p>
+        </div>
+        <div class="flex flex-wrap -m-4">
+
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">QUẢN LÝ MÔN HỌC</h4>
+                <a href="../subject">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://d1e4pidl3fu268.cloudfront.net/e37de9f7-b308-494f-8bb0-5dfbf9454336/ScreenShot20191127at155249.png">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">BẢNG
+                                THÔNG
+                                TIN MÔN HỌC
+                            </h2>
+                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">MÔN HỌC</h1>
+                            <p class="leading-relaxed">Nhấn vào để thực hiện chỉnh sửa thông tin môn học.</p>
+                            <p> </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">QUẢN LÝ GIÁO VIÊN</h4>
+                <a href="../teacher">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://png.pngtree.com/thumb_back/fw800/back_our/20190622/ourmid/pngtree-english-teacher-class-banner-background-image_226677.jpg">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">BẢNG
+                                THÔN
+                                TIN GIÁO VIÊN
+                            </h2>
+                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">GIÁO VIÊN</h1>
+                            <p class="leading-relaxed">Nhấn vào để thực hiện chỉnh sửa thông tin của giáo viên.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">QUẢN LÝ KHOA</h4>
+                <a href="../faculty">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://image.freepik.com/free-vector/isometric-business-people-meeting_52683-24992.jpg">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">BẢNG
+                                THÔNG
+                                TIN KHOA
+                            </h2>
+                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">KHOA</h1>
+                            <p class="leading-relaxed">Nhấn vào để thực hiện chỉnh sửa thông tin của khoa.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">QUẢN LÝ SINH VIÊN</h4>
+                <a href="../student">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://png.pngtree.com/thumb_back/fw800/back_pic/03/97/17/5157ef6a38630a9.jpg">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">BẢNG
+                                THÔNG
+                                TIN SINH VIÊN
+                            </h2>
+                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">SINH VIÊN</h1>
+                            <p class="leading-relaxed">Nhấn vào để thực hiện chỉnh sửa thông tin của sinh viên.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">QUẢN LÝ ĐIỂM SỐ</h4>
+                <a href="../point">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://img.freepik.com/free-vector/credit-score-mobile-application-with-rating-scale-from-bad-good-rate-vector-banner-with-cartoon-illustration-with-loan-meter-smartphone-screen-graph-money_107791-4210.jpg?size=626&ext=jpg&ga=GA1.2.1761048346.1623628800">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">BẢNG
+                                THÔNG
+                                TIN ĐIỂM SỐ
+                            </h2>
+                            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">ĐIỂM SỐ</h1>
+                            <p class="leading-relaxed">Nhấn vào để thực hiện chỉnh sửa thông tin điểm số.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            
+            <!-- ------------------------------------------------------------------------------------------------------- -->
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">Thống kê xếp hạng sinh viên</h4>
+                <a href="../statistical_Rank">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://thumbs.dreamstime.com/b/children-education-banner-cartoon-vector-illustration-learning-kids-funny-experiments-creative-space-back-to-school-science-89575034.jpg">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                            </h2>                        
+                            <p class="leading-relaxed">Nhấn vào để xem xếp hạng sinh viên.</p>
+                            <p> </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">Thống Kê số lượng sinh viên theo khoa</h4>
+                <a href="../statistical_Stu_faculty">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://i.pinimg.com/originals/f4/b2/46/f4b2466f1600ae3d5c8699d5adf77292.jpg">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                            </h2>
+                            <p class="leading-relaxed">Nhấn vào để xem Thống Kê số lượng sinh viên theo khoa.</p>
+                            <p> </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">Thống Kê số lượng môn học theo khoa</h4>
+                <a href="../statistical_Subj_faculty">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://png.pngtree.com/thumb_back/fw800/back_our/20190619/ourmid/pngtree-cartoon-education-background-event-advertising-poster-image_139214.jpg">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                            </h2>
+                            <p class="leading-relaxed">Nhấn vào để xem Thống Kê số lượng môn học theo khoa.</p>
+                            <p> </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">Thống Kê điểm trung bình theo lớp</h4>
+                <a href="../statistical_AVG_class">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://static.vecteezy.com/system/resources/previews/002/301/211/original/school-children-books-backpack-apple-pencil-boy-and-girl-student-with-study-supplies-back-to-school-banner-template-with-a-modern-abstract-background-vector.jpg">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                            </h2>
+                            <p class="leading-relaxed">Nhấn vào để xem Thống Kê điểm trung bình theo lớp.</p>
+                            <p> </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">Thống Kê điểm thi TB sinh viên có lần thi cuối cùng</h4>
+                <a href="../statistical_AVG_last">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTIOi5X_g1BKLuK1bfU7b5_aBXALTeMc8oLQ&usqp=CAU">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                            </h2>
+                            <p class="leading-relaxed">Nhấn vào để xem điểm thi TB sinh viên có lần thi cuối cùng.</p>
+                            <p> </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">Thống kê điểm của sinh viên đạt điểm 8 trở lên của mỗi khoa</h4>
+                <a href="../statistical_Stu_8">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://static.vecteezy.com/system/resources/thumbnails/002/538/658/small/funny-students-and-school-building-vector.jpg">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                            </h2>
+                            <p class="leading-relaxed">Nhấn vào để xem Thống kê điểm của sinh viên đạt điểm 8 trở lên của mỗi khoa.</p>
+                            <p> </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
+                <h4 class="font-bold">Thống kê điểm của sinh viên đạt dưới 5 của mỗi khoa</h4>
+                <a href="../statistical_Stu_5">
+                    <div class="flex relative h-52">
+                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center"
+                            src="https://media.slidesgo.com/storage/55896/upload.png">
+                        <div
+                            class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                            </h2>
+                            <p class="leading-relaxed">Nhấn vào để xem Thống kê điểm của sinh viên đạt dưới 5 của mỗi khoa.</p>
+                            <p> </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
